75d578f042c6e8396fa32c09e0a6d3be5c7073a4
Sajan Singh Shergill <ss865268@gmail.com>
2025-05-28 14:21:43 -0400

    code

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,17 @@
 const roles = ["Data Analyst", "Data Scientist"];
-const typingSpeed = 100;     // Typing delay
-const erasingSpeed = 50;     // Erasing delay
-const delayBetweenWords = 1000; // Delay between words
+const typingSpeed = 100;
+const erasingSpeed = 50;
+const delayBetweenWords = 1000;
+
 let roleIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
+
 const textElement = document.getElementById("typing-text");
 
 function typeEffect() {
   const currentRole = roles[roleIndex];
-  
+
   if (!isDeleting && charIndex <= currentRole.length) {
     textElement.textContent = currentRole.substring(0, charIndex++);
     setTimeout(typeEffect, typingSpeed);
